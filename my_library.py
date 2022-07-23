c2aa852534590db8e411ee03784150040c060d5d
Mikhail <a104s@mail.ru>
2022-07-24 00:58:06 +0300

    Add my_lib

---
+++ my_library.py
@@ -0,0 +1,35 @@
+def enter_digit(text:str):
+    '''
+    Проверяет ввод пользователя на число
+
+    :params: Принимает интерфейс пользователя в виде текстового сообщения
+
+    :return: Возвращает число типа float
+    '''
+    while True:
+        flag_negative_digit = 1
+        number = input(text)
+        if number.startswith('-'):
+            number = number[1:]
+            flag_negative_digit = -1
+        if number.count('.') == 1:
+            number_float = number.replace('.', '')
+            if number_float.isdigit():
+                break
+        if number.isdigit():
+            break
+        else:
+            print('Введенный символ не число! Повторите ввод.')
+    return float(number)*flag_negative_digit
+
+
+def enter_candy(text:str):
+    while True:
+        number = input(text)
+        if number.startswith('-') or number.count('.') == 1:
+            print('Число должно быть целое от 1 до 28! Повторите ввод.')
+        if number.isdigit() and 0 < int(number) < 29:
+            break
+        else:
+            print('Введенный символ вне диапазона, либо не число! Повторите ввод.')
+    return int(number)
